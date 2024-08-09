66f378f452918721e6713e645d4e14ef14b7b830
Joaquin Llenado <joaquinllenado@gmail.com>
2024-08-08 22:10:49 -0700

    added calls for HOKA and On brands

--- sneakr-server.js
+++ sneakr-server.js
@@ -8,6 +8,16 @@ const sneaks = new SneaksAPI();
 app.use(cors());
 app.use(express.json());
 
+/* app.get('/search', (req, res) => {
+    const query = req.query.q;
+    sneaks.getProducts(query, 20, function(err, products) {
+        if (err) {
+            return res.status(500).json({ error: 'Something went wrong.' });
+        }
+        res.json(products);
+    })
+}) */
+
 app.get('/mostpopular', (req, res) => {
     sneaks.getMostPopular(10, function(err, products){
         res.json(products);
@@ -56,7 +66,17 @@ app.get('/converse', (req, res) => {
     })
 })
 
+app.get('/hoka', (req, res) => {
+    sneaks.getProducts("HOKA", 20, function(err, products){
+        res.json(products);
+    })
+})
 
+app.get('/on', (req, res) => {
+    sneaks.getProducts("On Running Cloud", 20, function(err, products){
+        res.json(products);
+    })
+})
 
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`)
